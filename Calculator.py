cab768fb932413b77d3093bb8558952c07357ed1
margarsie <margarsie@gmail.com>
2020-02-24 12:05:37 +0100

    added apply urbag

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,9 @@
 class Calculator:
-    urbag_constant = 12.4
+    
     def sum(a,b):
         return a+b
+    
+    def applyUrbag(a,b):
+        urbag_constant = 12.4
+        result = a*urbag_constant+b
+        return result
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 from Calculator import Calculator
 
 result = Calculator.sum(3,4)
+
+result2 = Calculator.applyUrbag(3,4)
+print(result2)
